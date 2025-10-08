ab665bb1e4f77584cc90fbd41c7c114af3548514
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-08 11:14:11 +0200

    Autó modul és a gombok használatban

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Hello from './Hello'
+import Auto from './Auto'
 import Gomb1 from './Gomb1'
 import Gomb2 from './Gomb2'
 import './App.css'
@@ -18,6 +18,9 @@ function App() {
         </p>
         <Gomb1></Gomb1>
         <Gomb2></Gomb2>
+        <Auto color="piros" type="Jó" brand="Opel"></Auto>
+        <Auto color="sárga" type="Közepes" brand="Wolkswagen"></Auto>
+        <Auto color="kék" type="Rossz" brand="Renault"></Auto>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
