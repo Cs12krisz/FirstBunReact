7e9b1b5587ec26c29f689cb2add155e3c49af3cb
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-08 11:17:19 +0200

    Delaer modul létrehozva

---
+++ src/Dealer.jsx
@@ -0,0 +1,8 @@
+function Dealer({name, location, zip}) {
+    return(
+        <>
+        <p>A Dealer neve: {name}, helyszíne: {location}, zip: {zip}</p>
+        </>
+    )
+}
+export default Dealer;
